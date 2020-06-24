0a093f70e7c751570aaf6c0be4c9d241f610418b
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2020-06-24 11:37:33 +0100

    Add 5 vowel words

--- src/containers/NetworkedQuickEdit/NetworkedQuickEdit.tsx
+++ src/containers/NetworkedQuickEdit/NetworkedQuickEdit.tsx
@@ -4,12 +4,14 @@ import QuickEdit from '../../components/QuickEdit/QuickEdit';
 import { getCardbox, addWords } from '../../services/cardboxService';
 import highprob from '../../data/highprob.json';
 import highprob9 from '../../data/highprob9.json';
+import fiveVowels from '../../data/5vowels.json';
 
 const NetworkedQuickEdit: React.FC = () => {
 
     const actions = [
         {name: 'Add to High Probability', action: addToHighProb},
         {name: 'Add to High Probability Nines', action: addToHighProb9},
+        {name: 'Add to 5 Vowels', action: addTo5Vowels}
     ];
 
     return <QuickEdit actions={actions}></QuickEdit>
@@ -27,6 +29,13 @@ const addToHighProb9 = () =>
         const questionCount = cardbox.size;
         const wordsToAdd = highprob9.slice(questionCount, questionCount + 100).flatMap(ws => ws);
         return addWords('High Probability Nines', wordsToAdd);
+    })
+
+const addTo5Vowels = () =>
+    getCardbox('5 Vowels').then(cardbox => {
+        const questionCount = cardbox.size;
+        const wordsToAdd = fiveVowels.slice(questionCount, questionCount + 100).flatMap(ws => ws);
+        return addWords('5 Vowels', wordsToAdd);
     })
 
 export default NetworkedQuickEdit;
