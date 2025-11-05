4fe4609b501f5a33c676e42c9aec4dee7642f9f5
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2025-11-05 20:05:12 +0000

    Bugfix

--- src/services/cardboxService.ts
+++ src/services/cardboxService.ts
@@ -81,11 +81,8 @@ export const addWords = (cardbox: string, offset: number, words: string[]): Prom
                     transaction.update(cardboxRef, { size: fb.firestore.FieldValue.increment(1), words: fb.firestore.FieldValue.arrayUnion(...wordsByKey[key])});
                     transaction.set(docRef, { asked: 0, answeredCorrectly: 0, level: 0, due: fb.firestore.Timestamp.fromDate(dueDate) });
                 } else {
-                    const existingAnswers = (doc.data() as Question).answers;
-                    if (wordsByKey[key].some(ans => !existingAnswers.includes(ans))) {
-                        transaction.update(cardboxRef, { words: fb.firestore.FieldValue.arrayUnion(wordsByKey[key])});
-                        transaction.update(docRef, { due: fb.firestore.Timestamp.fromDate(dueDate) });
-                    }
+                    transaction.update(cardboxRef, { words: fb.firestore.FieldValue.arrayUnion(wordsByKey[key])});
+                    transaction.update(docRef, { due: fb.firestore.Timestamp.fromDate(dueDate) });
                 }
             })
         })
